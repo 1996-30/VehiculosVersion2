278e39d829245e0ed0af4e477f7c86df4a5fc829
Heber Monzon <hebermonzon@hotmail.com>
2024-10-10 20:19:27 -0600

    Actualizacion

---
+++ bd.php
@@ -0,0 +1,15 @@
+<?php
+$conn = mysqli_connect(
+    '88.223.84.162',
+    'u244043183_renta_vehiculo',
+    'Renta_Vehiculos2024',
+    'u244043183_renta_vehiculo'
+);
+/*
+if ($conn) {
+    echo "Conexión exitosa"; 
+} else {
+    echo "Error de conexión: " . mysqli_connect_error();
+}
+*/
+?>
